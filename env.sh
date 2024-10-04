a895e9eb0eb8adbf4e71b6245f6215eea40147e7
aaron <ahack210@gmail.com>
2024-10-04 08:45:35 -0500

    remove env.sh

--- env.sh
+++
@@ -1,8 +0,0 @@
-export BASE_DIR=/home/user/scc_telegraf
-export INFLUXDB_URL=http://10.10.10.10:8086
-export INFLUX_TOKEN=<INFLUX TOKEN>
-export INFLUX_BUCKET=ftd_telegraf
-export INFLUX_ORG_NAME=hacksbrain
-export SCC_BASE_URL=edge.us.cdo.cisco.com
-export SCC_TOKEN=<SCC API TOKEN>
-export FMC_UID=90a0edae-dd50-44f7-8dcf-c77e3612ccdf
